2b7fcdd85b911522d2efe8c41515da5914713f0d
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-13 12:44:39 -0400

    installed express-validator dependency, user registration completed with email validation, password encrpytion, and jsonwebtoken storage

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
+require("dotenv").config();
 const Express = require("express");
 const app = Express();
 const dbConnection = require("./db");
+app.use(require('./middleware/headers'));
 const controllers = require("./controllers");
 
+app.use(Express.json());
 app.use("/merchandise", controllers.merchandiseController);
-app.use("user", controllers.userController);
+app.use("/user", controllers.userController);
+app.use("/shop", controllers.shopController);
 
 dbConnection.authenticate()
     .then(() => dbConnection.sync())
