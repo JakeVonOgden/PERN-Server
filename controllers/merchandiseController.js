e8627f3c6668f855da0fdf617f70e0ac998ac53a
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-13 13:22:29 -0400

    Validate-JWT logic added, succesfully able to protect merchandise test route

--- controllers/merchandiseController.js
+++ controllers/merchandiseController.js
@@ -1,4 +1,11 @@
 const router = require("express").Router();
+const validateJWT = require("../middleware/validate-jwt");
 const { MerchandiseModel } = require('../models');
+
+router.get("/test", validateJWT, (req, res) => {
+    res.send("hey! this is the test route!")
+});
+
+
 
 module.exports = router;
