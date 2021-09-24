d7eb9e98dfa2a5238444cc8111ec7356b9f15fff
Jacob Von Ogden <jvon1197@gmail.com>
2021-09-23 20:12:39 -0400

    fixed typo

--- db.js
+++ db.js
@@ -1,8 +1,13 @@
-  const {Sequelize} = require("sequelize")
+const {Sequelize} = require("sequelize")
 
 const db = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
-    ssl: process.env.ENVIRONMENT === 'production'
+    ssl: process.env.ENVIRONMENT === 'production',
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: false
+        }
+    }
 });
 
 module.exports = db;
